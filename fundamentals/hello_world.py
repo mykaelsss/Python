8f9f0ddfc1dc58223c7d848e77a326646efdec11
Mykaelsss <mykaelsicard04@gmail.com>
2022-09-26 19:46:17 -0400

    for loops basics 1

--- fundamentals/hello_world.py
+++ fundamentals/hello_world.py
@@ -1,15 +1,62 @@
-# 1. TASK: print "Hello World"
-print("Hello World")
-# 2. print "Hello Noelle!" with the name in a variable
-name = "Noelle"
-print( "Hello ",name)	# with a comma
-print( "Hello " + name )	# with a +
-# 3. print "Hello 42!" with the number in a variable
-name = 42
-print( "Hello" , name)	# with a comma
-# print( "Hello " + name)	# with a +	-- this one should give us an error!
-# 4. print "I love to eat sushi and pizza." with the foods in variables
-fave_food1 = "sushi"
-fave_food2 = "pizza"
-print("I love to eat {} and {}".format(fave_food1, fave_food2) ) # with .format()
-print( f"I love to eat {fave_food1} and {fave_food2}" ) # with an f string
+# # 1. TASK: print "Hello World"
+# print("Hello World")
+# # 2. print "Hello Noelle!" with the name in a variable
+# name = "Noelle"
+# print( "Hello ",name)	# with a comma
+# print( "Hello " + name )	# with a +
+# # 3. print "Hello 42!" with the number in a variable
+# name = 42
+# print( "Hello" , name)	# with a comma
+# # print( "Hello " + name)	# with a +	-- this one should give us an error!
+# # 4. print "I love to eat sushi and pizza." with the foods in variables
+# fave_food1 = "sushi"
+# fave_food2 = "pizza"
+# print("I love to eat {} and {}".format(fave_food1, fave_food2) ) # with .format()
+# print( f"I love to eat {fave_food1} and {fave_food2}" ) # with an f string
+
+
+# some_nums = [44,56,2,3,12,19,6]
+# print("Get started by writing your own code!")
+# print(len(some_nums))
+# print(some_nums.index(12))
+# print(some_nums)
+# some_nums.pop(5)
+# print(some_nums)
+# some_nums.reverse()
+# print(some_nums)
+# # Some optional challenges to assess and refine your understanding:
+
+# # Print the length of the list.
+
+# # Use antoher python built-in function and print the result.
+
+# # Remove an item from the list. Remember to verify that it was removed.
+
+# # Utilize a method from the documentation and print the result.
+
+
+# # for x in range(0, 10, 2):
+# #     print(x)
+# # # output: 0, 2, 4, 6, 8
+# # for x in range(5, 1, -3):
+# #     print(x)
+# # # output: 5, 2
+# for x in range(1, 21):
+#     print(x)
+# # Challenge: Write a for loop to print all integers from 1 to 20, including 20.
+
+
+
+# countries = ["Uganda", "Chile", "Albania", "Saudi Arabia"]
+
+# # Challenge 1: Fix the range!
+# for integer in range(0, len(countries)):
+#      print("Index:", countries.index(countries[integer]))
+#      # Challenge 2: print the index here
+#      print("Country:", countries[integer] )
+# #     # Challenge 3: print the country here
+
+# Looping over values only...
+# for country in countries:
+#     print("Country: " , country)
+    # Challenge 4: print the country here
---
+++ fundamentals/introduction/for_loop_basic1.py
@@ -0,0 +1,36 @@
+
+# for i in range(151):
+#     print(i)
+
+
+# for i in range(5, 1000):
+#     if(i % 5 == 0 ):
+#         print(i)
+
+
+# for i in range(1, 101):
+#     if( i % 10 == 0):
+#         print("Coding Dojo")
+#     elif( i % 5 == 0 ):
+#         print("Coding")
+#     else:
+#         print(i)
+
+
+# sum = 0
+# for i in range(500000):
+#     if( i % 2 == 1 ):
+#         sum = i + sum
+
+# print(sum)
+
+# for i in range(2018, 0 ,-4):
+#     print(i)
+
+
+lowNum = 4
+highNum = 15
+mult = 5
+for i in range(lowNum, highNum+1):
+    if ( i % mult == 0):
+        print(i)
