4a4b83bc33aa6f6b90d7a28aca4b9b92e219e0ff
Mykaelsss <mykaelsicard04@gmail.com>
2022-09-27 18:56:05 -0400

    bank account assingment

---
+++ fundamentals/opp/bank-account.py
@@ -0,0 +1,32 @@
+class BankAccount:
+    # don't forget to add some default values for these parameters!
+
+    def __init__(self, int_rate, balance):
+        self.balance = 0
+        self.int_rate = 0.05
+
+    def deposit(self, amount):
+        self.balance += amount
+        return self
+
+    def withdraw(self, amount):
+        if self.balance >= amount:
+            self.balance -= amount
+        else:
+            print("Insufficient funds: charging a $5 fee")
+            self.balance -= 5
+        return self
+
+    def display_account_info(self):
+        print(f"Balance: {self.balance}")
+        return self
+
+    def yield_interest(self):
+        if self.balance > 0:
+            self.balance += self.balance * self.int_rate
+        return self
+
+
+b1 = BankAccount(0, 0).deposit(50).deposit(100).deposit(400).withdraw(150).yield_interest().display_account_info()
+
+b2 = BankAccount(0, 0).deposit(20).deposit(100).withdraw(10).withdraw(20).withdraw(10).withdraw(30).yield_interest().display_account_info()
