d013256c76592d8e84f7723e2ac1c941c1bd9518
Mykaelsss <mykaelsicard04@gmail.com>
2022-09-27 20:21:16 -0400

    user bank account

--- fundamentals/opp/user_bank_account.py
+++ fundamentals/opp/user_bank_account.py
@@ -1,3 +1,27 @@
+
+
+
+class User:
+    def __init__(self, name , email):
+        self.first = name
+        self.email = email
+        self.account = BankAccount(0.02, 0)
+
+    def display_info(self,name):
+        print(self.name)
+
+    def make_depoist(self):
+        self.account.deposit(400)
+        return self
+
+    def make_withdrawal(self):
+        self.account.withdraw(200)
+        return self
+
+    def display_user_balance(self):
+        self.account.display_account_info()
+        return self
+
 class BankAccount:
     # don't forget to add some default values for these parameters!
 
@@ -27,8 +51,7 @@ class BankAccount:
         return self
 
 
-b1 = BankAccount(.05, 400).deposit(50).deposit(100).deposit(400).withdraw(150).yield_interest().display_account_info()
 
-b2 = BankAccount(.03, 300).deposit(20).deposit(100).withdraw(10).withdraw(20).withdraw(10).withdraw(30).yield_interest().display_account_info()
-
-b1.display_account_info()
+# checkings = User("Mykael Sicard" , "mykaels@gmail.com" )
+# checkings.make_depoist().make_withdrawal().display_user_balance()
+# savings = User()
